1250a29610e397b7860116664ad64f72329ffc8b
kinfe mulugeta <kinfe2534@gmail.com>
2021-07-22 16:18:13 +0300

    drawer initial commit

---
+++ lib/widgets/menu_list_tile.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import '../pages/birthdays.dart';
+import '../pages/gratitude.dart';
+import '../pages/reminders.dart';
+
+class MenuListTileWidget extends StatefulWidget {
+  const MenuListTileWidget({Key? key}) : super(key: key);
+
+  @override
+  _MenuListTileWidgetState createState() => _MenuListTileWidgetState();
+}
+
+class _MenuListTileWidgetState extends State<MenuListTileWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        ListTile(
+          leading: Icon(Icons.cake),
+          title: Text('Birthdays'),
+          onTap: () {
+            Navigator.pop(context);
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => Birthdays(),
+              ),
+            );
+          },
+        ),
+        ListTile(
+          leading: Icon(Icons.sentiment_satisfied),
+          title: Text('Gratitude'),
+          onTap: () {
+            Navigator.pop(context);
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => Gratitude(),
+              ),
+            );
+          },
+        ),
+        ListTile(
+          leading: Icon(Icons.alarm),
+          title: Text('Reminders'),
+          onTap: () {
+            Navigator.pop(context);
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => Reminders(),
+              ),
+            );
+          },
+        ),
+        Divider(color: Colors.grey),
+        ListTile(
+          leading: Icon(Icons.settings),
+          title: Text('Setting'),
+          onTap: () {
+            Navigator.pop(context);
+          },
+        ),
+      ],
+    );
+  }
+}
